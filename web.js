8012c4fb11495c78a9cf243b096b311a15b0aeed
Varun Phadnis <varun.phadnis@gmail.com>
2013-07-10 01:09:42 +0530

    used buffer API

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	fs.readFileSync("index.html", function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
-	});
+	var buf = new Buffer (fs.readFileSync("index.html", "utf-8"));
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write(buf.toString());
+	response.end();
+	
 });
 
 var port = process.env.PORT || 5000;
