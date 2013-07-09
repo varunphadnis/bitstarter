06ae71a61064bce37ce99904fd5bae73564146c9
Varun Phadnis <varun.phadnis@gmail.com>
2013-07-10 01:01:23 +0530

    modified to read index page

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs')
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+	fs.readFileSync("index.html", function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+	});
 });
 
 var port = process.env.PORT || 5000;
